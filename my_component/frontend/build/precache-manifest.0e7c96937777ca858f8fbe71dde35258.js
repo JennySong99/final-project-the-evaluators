42b02540109bda62da12daded615db2d58d7865c
jenny <jingyi_song@berkeley.edu>
2022-04-28 00:27:36 -0400

    added build and venv for deployment

---
+++ my_component/frontend/build/precache-manifest.0e7c96937777ca858f8fbe71dde35258.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b1585397de015559698ccb8d0e3fd9e",
+    "url": "./index.html"
+  },
+  {
+    "revision": "fffcce3cff79e180b24e",
+    "url": "./static/js/2.bed93c76.chunk.js"
+  },
+  {
+    "revision": "3fc7fb5bfeeec1534560a2c962e360a7",
+    "url": "./static/js/2.bed93c76.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f898a7373d88d98a9181",
+    "url": "./static/js/main.d648220a.chunk.js"
+  },
+  {
+    "revision": "7c26bca7e16783d14d15",
+    "url": "./static/js/runtime-main.11ec9aca.js"
+  }
+]);
